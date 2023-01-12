79594ba22beabfec9338e400dce98ff6bc874248
Wilkins Morales <wilkinscom@gmail.com>
2023-01-12 22:38:49 +0200

    init race car plus iteration 1

--- js/index.js
+++ js/index.js
@@ -1,7 +1,22 @@
 window.onload = () => {
+  
+
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
+  
 
-  function startGame() {}
+  function startGame() {
+    const canvas = document.getElementById('canvas')
+    const ctx = canvas.getContext('2d')
+    const road = new Image()
+      road.src = "./../images/road.png"
+    road.onload = () => {
+      console.log("loading")
+      ctx.drawImage(road, 0, 0 , 500 ,700);
+      ctx.beginPath();
+      ctx.stroke();
+  }
 };
+
+}
