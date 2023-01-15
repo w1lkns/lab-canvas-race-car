4b5458f51830c02f1027e189440b6afa4c34d8c9
Wilkins Morales <wilkinscom@gmail.com>
2023-01-15 21:31:50 +0200

    "fix iterations 1 and 2"

--- js/index.js
+++ js/index.js
@@ -1,29 +1,54 @@
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+
+class Component{
+  constructor(width,height,x,y,source,scale){
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    this.scale = scale;
+    this.image = new Image();
+    this.image.src = source;
+  }
+
+  draw(){
+    ctx.drawImage(this.image, this.x, this.y, this.image.width / this.scale, this.image.height / this.scale);
+  }
+
+  update() {
+    if (this.image) {
+      ctx.drawImage(this.image, this.x, this.y, this.image.width / this.scale, this.image.height / this.scale);
+    }
+  }
+}
+
+const player = new Component(30,75,233,600,"./../images/car.png",4)
+
+
+function startGame() {
+  const road = new Image()
+  road.src = "./../images/road.png"
+  road.onload = () => {
+    ctx.drawImage(road, 0, 0 , canvas.width ,canvas.height);
+    player.draw();
+  }
+
+};
+
 window.onload = () => {
   
 
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
+  
 
-  function startGame() {
-    const canvas = document.getElementById('canvas')
-    const ctx = canvas.getContext('2d')
-    const road = new Image()
-      road.src = "./../images/road.png"
-    const car = new Image()
-      car.src = "./../images/car.png"
-    car.onload = () => {
-      console.log('loading')
-      ctx.drawImage(car,233,600,30,75)
-      ctx.beginPath();
-      ctx.stroke();
-    }
-    road.onload = () => {
-      console.log("loading")
-      ctx.drawImage(road, 0, 0 , 500 ,700);
-      ctx.beginPath();
-      ctx.stroke();
-  }
-};
-
+ 
 }
+
+/*
+*/
+
+
+console.log(player)
