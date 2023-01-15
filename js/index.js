88f3e914d82c328132b0fd59072dcc230e7bc052
Wilkins Morales <wilkinscom@gmail.com>
2023-01-15 19:36:13 +0200

    "Iteration 1 and 2"

--- js/index.js
+++ js/index.js
@@ -4,13 +4,20 @@ window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
-  
 
   function startGame() {
     const canvas = document.getElementById('canvas')
     const ctx = canvas.getContext('2d')
     const road = new Image()
       road.src = "./../images/road.png"
+    const car = new Image()
+      car.src = "./../images/car.png"
+    car.onload = () => {
+      console.log('loading')
+      ctx.drawImage(car,233,600,30,75)
+      ctx.beginPath();
+      ctx.stroke();
+    }
     road.onload = () => {
       console.log("loading")
       ctx.drawImage(road, 0, 0 , 500 ,700);
